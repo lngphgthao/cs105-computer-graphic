9b1dd08e65f384d04f27ac514f2d9da435c61236
Le Ngoc Phuong Thao <phuongthao27102005@gmail.com>
2026-05-25 10:49:27 +0700

    feat: Add background music, footsteps and make the lighting, audio adjustable

--- src/core/scene.js
+++ src/core/scene.js
@@ -3,8 +3,8 @@ import * as THREE from "three";
 export function createScene() {
 	const scene = new THREE.Scene();
 
-	scene.background = new THREE.Color(0xc9e6ff);
-	scene.fog = new THREE.Fog(0xc9e6ff, 25, 120);
+	scene.background = new THREE.Color(0xbfdcff);
+	scene.fog = new THREE.Fog(0xbfdcff, 18, 95);
 
 	return scene;
 }
